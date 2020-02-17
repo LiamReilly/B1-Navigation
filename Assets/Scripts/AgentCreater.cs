ed898913923f43d2c9bb5549da80c180b7b2dbbf
LiamReilly <liamreilly22@yahoo.com>
2020-02-16 21:04:37 -0500

    Created the repository

---
+++ Assets/Scripts/AgentCreater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentCreater : MonoBehaviour
+{
+    public float radius;
+    public GameObject agent;
+    public int agentCount;
+    Vector3 location;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var Count = 0;
+        radius = radius * 2;
+        while (Count < agentCount)
+        {
+            location = new Vector3((Random.value-0.5f) * radius, 0.5f, (Random.value-0.5f) * radius);
+            var newAgent = Instantiate(agent, location, Quaternion.identity);
+            
+            //newAgent.transform.position = new Vector3((Random.value - 0.5f) * radius, 0.5f, ((Random.value - 0.5f)) * radius);
+            newAgent.name = "a" + Count;
+            
+            Count++;
+        }
+    }
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private int speed=3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            gameObject.transform.Translate(Vector3.forward * Time.deltaTime*speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            gameObject.transform.Translate(Vector3.back * Time.deltaTime*speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.Translate(-Vector3.right * Time.deltaTime*speed);
+        }
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime*speed);
+        }
+        if (Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.RightShift))
+        {
+            gameObject.transform.Translate(Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.transform.Translate(Vector3.down * Time.deltaTime);
+        }
+    }
+}
